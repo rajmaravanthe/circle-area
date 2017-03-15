a6098fa15521cc50a0fa497df9c5ccb7b6310a62
rajesha maravanthe <rraajjpoojary@gmail.com>
2017-03-15 10:57:25 +0530

    added files

---
+++ app.js
@@ -0,0 +1,5 @@
+
+
+var radius = parseFloat(prompt("Enter radius of circle")); // get the radius from user
+var area = 3.14159 * radius * radius; // area = pi*r*r (pi = 3.14159)
+document.write("Area of Circle = "+ area);
